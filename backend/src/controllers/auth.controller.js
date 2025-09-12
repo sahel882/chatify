aad634ac03f59eeeb6c828fabd5dfbbc31731f39
maneshadesilva99-code <maneshadesilva99@gmail.com>
2025-09-12 18:43:04 +0530

    Welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import { ENV } from "../lib/env.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
@@ -45,7 +47,14 @@ export const signup = async (req, res) => {
                     email: newUser.email,
                     profilePic: newUser.profilePic
                 }
-            })
+            });
+
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            } catch (error) {
+                console.error("Failed to send welcome email: ", error);
+            }
+
         } else {
             res.status(400).json({ message: "Invalid user data" });
         }
