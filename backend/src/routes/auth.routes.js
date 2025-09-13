07cbb6e17994a2a0779f982094412dd22ce861a6
maneshadesilva99-code <maneshadesilva99@gmail.com>
2025-09-13 07:28:57 +0530

    update profile endpoint added

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile, } from "../controllers/auth.controller.js";
+import { protectedRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,9 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile", protectedRoute, updateProfile);
+
+router.get('/check', protectedRoute, (req, res) => res.status(200).json(req.user));
 
 export default router;
