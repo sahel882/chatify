2a5e7821339e1107a5141275c0362341016d10c7
maneshadesilva99-code <maneshadesilva99@gmail.com>
2025-09-16 18:26:56 +0530

    sidebar cmpleted

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,36 +1,33 @@
-import React from 'react'
+import React from 'react';
 import { useChatStore } from '../store/useChatStore';
 import BorderAnimatedContainer from '../components/BorderAnimatedContainer';
 import ProfileHeader from '../components/ProfileHeader';
 import ActiveTabSwitch from '../components/ActiveTabSwitch';
-import ChatList from '../components/ChatList';
 import ContactList from '../components/ContactList';
 import NoConversationPlaceholder from '../components/NoConversationPlaceholder';
 import ChatContainer from '../components/ChatContainer';
+import ChatsList from '../components/ChatList';
 
 function ChatPage() {
-
-  const { activeTab, selectedUser } = useChatStore;
+  const { activeTab, selectedUser } = useChatStore();
 
   return (
     <div className='relative w-full max-w-6xl h-[800px]'>
       <BorderAnimatedContainer>
         <div className='w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col'>
           <ProfileHeader />
-          <h1>ContactList</h1>
           <ActiveTabSwitch />
           <div className='flex-1 overflow-y-auto p-4 space-y-2'>
-            {activeTab === "chats" ? <ChatList /> : <ContactList />}
+            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
           </div>
         </div>
 
         <div className='flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm'>
           {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
         </div>
-
       </BorderAnimatedContainer>
     </div>
-  )
+  );
 }
 
-export default ChatPage
+export default ChatPage;
