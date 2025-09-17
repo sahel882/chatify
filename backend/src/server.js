03b91ec47164ce7ad587df244632946f49a3cbac
maneshadesilva99-code <maneshadesilva99@gmail.com>
2025-09-17 16:29:53 +0530

    socket implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,8 +6,9 @@ import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from './lib/socket.js';
 
-const app = express();
+
 const __dirname = path.resolve();
 const PORT = ENV.PORT || 4000;
 
@@ -28,7 +29,7 @@ if (ENV.NODE_ENV === "production") {
     });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server running on port: " + PORT);
     connectDB();
 });
